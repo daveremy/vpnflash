521a0519487e3c47b81d8f60356c5c4e1b1b26ce
Dave Remy <email@daveremy.com>
2015-04-07 17:48:35 -0700

    Initial push

---
+++ VpnFlash/FlashWindowForm.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace VpnFlash
+{
+    partial class FlashWindowForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FlashWindowForm));
+            this.label_vpn_status = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label_vpn_status
+            // 
+            this.label_vpn_status.AutoSize = true;
+            this.label_vpn_status.Location = new System.Drawing.Point(61, 50);
+            this.label_vpn_status.Name = "label_vpn_status";
+            this.label_vpn_status.Size = new System.Drawing.Size(125, 13);
+            this.label_vpn_status.TabIndex = 0;
+            this.label_vpn_status.Text = "Determing VPN Status ...";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(64, 123);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Minimize";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FlashWindowForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 174);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label_vpn_status);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FlashWindowForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_vpn_status;
+        private System.Windows.Forms.Button button1;
+
+
+
+    }
+}
+
